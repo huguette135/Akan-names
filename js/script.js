94c53dd370d329962c34a846f1164c3943357e57
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-09-25 19:05:49 +0200

    made change in formula

--- js/script.js
+++ js/script.js
@@ -3,19 +3,16 @@
         let birthday = document.getElementById('birthday').value;
 
         let century = getCentury(birthday);
-        let year = birthday.split("-")[0].substr(2,4);
+        let year = birthday.split("-")[0].substr(2);
         let month = birthday.split("-")[1];
         let day = birthday.split("-")[2];        
 
-        let weekDay =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7
+        let weekDay =  ( ( (century/4) -2*century-1) + ((5*Year/4) ) + ((26*(month+1)/10)) + day ) %7
         // let weekDay = century;
         
         // console.log(weekDay);
 
         alert("Hi " + weekDay);
 
-    }    
-    function getCentury(year) {
-        let value = Math.floor(year/100) + 1;
-        return value ;
-    }
+     
+    
