148d32e21ccd102e293757e72899c8be35d9b02f
UMUTONI Huguette <hyguetteumutoni@gmail.com>
2020-09-25 19:58:23 +0200

    adding some change

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@
     function setName() {
         let birthday = document.getElementById('birthday').value;
 
-        let century = getCentury(birthday);
-        let year = birthday.split("-")[0].substr(2);
-        let month = birthday.split("-")[1];
-        let day = birthday.split("-")[2];        
+        let century = parseInt(birthday.split("-")[0].substr(0,2));
+        let year = parseInt(birthday.split("-")[0].substr(2));
+        let month = parseInt(birthday.split("-")[1]);
+        let day = parseInt(birthday.split("-")[2]);        
 
-        let weekDay =  ( ( (century/4) -2*century-1) + ((5*Year/4) ) + ((26*(month+1)/10)) + day ) %7
+        let weekDay =  ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) %7
         
-        var boysname=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-        var girlsname=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+        var boysNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+        var girlsNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
     const rbs = document.getElementsByName("gender");
     let selectedValue;
     for (const rb of rbs) {
